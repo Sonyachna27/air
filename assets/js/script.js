7bd1c7c98cf49e52529365c5c2ec1da0f4c2a209
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-09-03 22:07:15 +0300

    add header

--- assets/js/script.js
+++ assets/js/script.js
@@ -240,57 +240,57 @@ const similarSwiper = () =>{
 	});
 }
 // якщо не використовувати функцію з модуля то ця для анімації хедеру
-const animationHeader = () => {
-	const airdropWrapperSections = document.querySelectorAll(".header__top-animation");
+// const animationHeader = () => {
+// 	const airdropWrapperSections = document.querySelectorAll(".header__top-animation");
 
-	airdropWrapperSections.forEach((airdropWrapperSection)=>{
+// 	airdropWrapperSections.forEach((airdropWrapperSection)=>{
 	
-					function setupInstances() {
-							const parentWidth = airdropWrapperSection.clientWidth;
-							const childWidth = airdropWrapperSection.children[0].offsetWidth;
-console.log(childWidth);
+// 					function setupInstances() {
+// 							const parentWidth = airdropWrapperSection.clientWidth;
+// 							const childWidth = airdropWrapperSection.children[0].offsetWidth;
+// console.log(childWidth);
 
-							if (!parentWidth || !childWidth) {
-									console.error("Cannot calculate parent or child width:", {
-											parentWidth,
-											childWidth,
-									});
-									return;
-							}
+// 							if (!parentWidth || !childWidth) {
+// 									console.error("Cannot calculate parent or child width:", {
+// 											parentWidth,
+// 											childWidth,
+// 									});
+// 									return;
+// 							}
 
-							const numInstances = Math.ceil(parentWidth / childWidth) + 1;
+// 							const numInstances = Math.ceil(parentWidth / childWidth) + 1;
 
-							while (airdropWrapperSection.children.length < numInstances) {
-									for (let i = 0; i < numInstances; i++) {
-											const clone = airdropWrapperSection.children[i].cloneNode(true);
-											clone.classList.remove("header__top-content");
-											clone.classList.add("header__top-content-second");
-											airdropWrapperSection.appendChild(clone);
-									}
-							}
-					}
+// 							while (airdropWrapperSection.children.length < numInstances) {
+// 									for (let i = 0; i < numInstances; i++) {
+// 											const clone = airdropWrapperSection.children[i].cloneNode(true);
+// 											clone.classList.remove("header__top-content");
+// 											clone.classList.add("header__top-content-second");
+// 											airdropWrapperSection.appendChild(clone);
+// 									}
+// 							}
+// 					}
 
-					setupInstances();
-					window.addEventListener("resize", setupInstances);
+// 					setupInstances();
+// 					window.addEventListener("resize", setupInstances);
 
-					const animationAirdropBlocks = document.querySelectorAll('.header__container-link');
-					const animationAirdropWrappers = document.querySelectorAll('.header__top-content');
-					const animationAirdropWrapperClones = document.querySelectorAll('.header__top-content-second');
+// 					const animationAirdropBlocks = document.querySelectorAll('.header__container-link');
+// 					const animationAirdropWrappers = document.querySelectorAll('.header__top-content');
+// 					const animationAirdropWrapperClones = document.querySelectorAll('.header__top-content-second');
 
-					animationAirdropBlocks.forEach((block) => {
-							block.addEventListener('mouseenter', () => {
-									animationAirdropWrappers.forEach(wrap => wrap.style.animationPlayState = "paused");
-									animationAirdropWrapperClones.forEach(clone => clone.style.animationPlayState = "paused");
-							});
+// 					animationAirdropBlocks.forEach((block) => {
+// 							block.addEventListener('mouseenter', () => {
+// 									animationAirdropWrappers.forEach(wrap => wrap.style.animationPlayState = "paused");
+// 									animationAirdropWrapperClones.forEach(clone => clone.style.animationPlayState = "paused");
+// 							});
 
-							block.addEventListener('mouseleave', () => {
-								animationAirdropWrappers.forEach(wrap => wrap.style.animationPlayState = "running");
-									animationAirdropWrapperClones.forEach(clone => clone.style.animationPlayState = "running");
-							});
-					});
+// 							block.addEventListener('mouseleave', () => {
+// 								animationAirdropWrappers.forEach(wrap => wrap.style.animationPlayState = "running");
+// 									animationAirdropWrapperClones.forEach(clone => clone.style.animationPlayState = "running");
+// 							});
+// 					});
 
-	})	
-}
+// 	})	
+// }
 
 
 const stickyName = () => {
@@ -317,3 +317,149 @@ const stickyName = () => {
 			window.addEventListener('scroll', handleScroll);
 	});
 };
+
+// const animationHeader = () =>{
+// 	const wrapper = document.querySelector(".wrapper");
+// const boxes = gsap.utils.toArray(".box");
+
+
+// const loop = horizontalLoop(boxes, {paused: false,repeat: -1,});
+
+// function horizontalLoop(items, config) {
+// 	items = gsap.utils.toArray(items);
+// 	config = config || {};
+// 	let tl = gsap.timeline({repeat: config.repeat, paused: config.paused, defaults: {ease: "none"}, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100)}),
+// 		length = items.length,
+// 		startX = items[0].offsetLeft,
+// 		times = [],
+// 		widths = [],
+// 		xPercents = [],
+// 		curIndex = 0,
+// 		pixelsPerSecond = (config.speed || .2) * 100,
+// 		snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
+// 		totalWidth, curX, distanceToStart, distanceToLoop, item, i;
+// 	gsap.set(items, { // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+// 		xPercent: (i, el) => {
+// 			let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
+// 			xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 100 + gsap.getProperty(el, "xPercent"));
+// 			return xPercents[i];
+// 		}
+// 	});
+// 	gsap.set(items, {x: 0});
+// 	totalWidth = items[length-1].offsetLeft + xPercents[length-1] / 100 * widths[length-1] - startX + items[length-1].offsetWidth * gsap.getProperty(items[length-1], "scaleX") + (parseFloat(config.paddingRight) || 0);
+// 	for (i = 0; i < length; i++) {
+// 		item = items[i];
+// 		curX = xPercents[i] / 100 * widths[i];
+// 		distanceToStart = item.offsetLeft + curX - startX;
+// 		distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+// 		tl.to(item, {xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond}, 0)
+// 		  .fromTo(item, {xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100)}, {xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false}, distanceToLoop / pixelsPerSecond)
+// 		  .add("label" + i, distanceToStart / pixelsPerSecond);
+// 		times[i] = distanceToStart / pixelsPerSecond;
+// 	}
+// 	function toIndex(index, vars) {
+// 		vars = vars || {};
+// 		(Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
+// 		let newIndex = gsap.utils.wrap(0, length, index),
+// 			time = times[newIndex];
+// 		if (time > tl.time() !== index > curIndex) { // if we're wrapping the timeline's playhead, make the proper adjustments
+// 			vars.modifiers = {time: gsap.utils.wrap(0, tl.duration())};
+// 			time += tl.duration() * (index > curIndex ? 1 : -1);
+// 		}
+// 		curIndex = newIndex;
+// 		vars.overwrite = true;
+// 		return tl.tweenTo(time, vars);
+// 	}
+// 	tl.next = vars => toIndex(curIndex+1, vars);
+// 	tl.previous = vars => toIndex(curIndex-1, vars);
+// 	tl.current = () => curIndex;
+// 	tl.toIndex = (index, vars) => toIndex(index, vars);
+// 	tl.times = times;
+//   tl.progress(1, true).progress(0, true); // pre-render for performance
+//   if (config.reversed) {
+//     tl.vars.onReverseComplete();
+//     tl.reverse();
+//   }
+// 	return tl;
+// }
+// }
+
+const animationHeader = () => {
+	const wrapper = document.querySelector(".wrapper");
+	const boxes = gsap.utils.toArray(".box");
+
+	const loop = horizontalLoop(boxes, { paused: false, repeat: -1 });
+
+	// Додаємо події миші для кожного елемента `.box`
+	boxes.forEach(box => {
+			box.addEventListener("mouseenter", () => {
+					loop.pause(); // Зупиняємо анімацію при наведенні мишки
+			});
+			box.addEventListener("mouseleave", () => {
+					loop.play(); // Відновлюємо анімацію при прибиранні мишки
+			});
+	});
+
+	function horizontalLoop(items, config) {
+			items = gsap.utils.toArray(items);
+			config = config || {};
+			let tl = gsap.timeline({
+							repeat: config.repeat,
+							paused: config.paused,
+							defaults: { ease: "none" },
+							onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100)
+					}),
+					length = items.length,
+					startX = items[0].offsetLeft,
+					times = [],
+					widths = [],
+					xPercents = [],
+					curIndex = 0,
+					pixelsPerSecond = (config.speed || 0.2) * 100,
+					snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1),
+					totalWidth, curX, distanceToStart, distanceToLoop, item, i;
+			gsap.set(items, { // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+					xPercent: (i, el) => {
+							let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
+							xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 100 + gsap.getProperty(el, "xPercent"));
+							return xPercents[i];
+					}
+			});
+			gsap.set(items, { x: 0 });
+			totalWidth = items[length - 1].offsetLeft + xPercents[length - 1] / 100 * widths[length - 1] - startX + items[length - 1].offsetWidth * gsap.getProperty(items[length - 1], "scaleX") + (parseFloat(config.paddingRight) || 0);
+			for (i = 0; i < length; i++) {
+					item = items[i];
+					curX = xPercents[i] / 100 * widths[i];
+					distanceToStart = item.offsetLeft + curX - startX;
+					distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+					tl.to(item, { xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond }, 0)
+							.fromTo(item, { xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100) }, { xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false }, distanceToLoop / pixelsPerSecond)
+							.add("label" + i, distanceToStart / pixelsPerSecond);
+					times[i] = distanceToStart / pixelsPerSecond;
+			}
+			function toIndex(index, vars) {
+					vars = vars || {};
+					(Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
+					let newIndex = gsap.utils.wrap(0, length, index),
+							time = times[newIndex];
+					if (time > tl.time() !== index > curIndex) { // if we're wrapping the timeline's playhead, make the proper adjustments
+							vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
+							time += tl.duration() * (index > curIndex ? 1 : -1);
+					}
+					curIndex = newIndex;
+					vars.overwrite = true;
+					return tl.tweenTo(time, vars);
+			}
+			tl.next = vars => toIndex(curIndex + 1, vars);
+			tl.previous = vars => toIndex(curIndex - 1, vars);
+			tl.current = () => curIndex;
+			tl.toIndex = (index, vars) => toIndex(index, vars);
+			tl.times = times;
+			tl.progress(1, true).progress(0, true); // pre-render for performance
+			if (config.reversed) {
+					tl.vars.onReverseComplete();
+					tl.reverse();
+			}
+			return tl;
+	}
+}
